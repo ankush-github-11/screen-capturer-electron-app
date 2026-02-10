7677a28ab2338b1bda46d71284159b12654031d0
ankush-github-11 <ankush10yt@gmail.com>
2026-02-10 21:40:32 +0530

    Wrapping for build

--- src/main/main.js
+++ src/main/main.js
@@ -19,6 +19,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
     show: true,
+    icon: path.join(__dirname, "../../assets/icon.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
